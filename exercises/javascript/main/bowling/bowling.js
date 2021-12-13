2b002ebab9348b34cc5655427570335ff256df31
Kallum Jones <kallum.jones@autotrader.co.uk>
2021-12-13 14:53:11 +0000

    Add third test for TDD and refactor

--- exercises/javascript/main/bowling/bowling.js
+++ exercises/javascript/main/bowling/bowling.js
@@ -1,10 +1,22 @@
 export const bowl = (rolls) => {
     let total = 0;
-    
-    for (let i = 0; i < rolls.length; i++) {
-      total += rolls[i];
+    const frames = [];
+  
+    for (let i = 0; i < rolls.length; i += 2) {
+      // total += rolls[i];
+  
+      frames.push([rolls[i], rolls[i + 1]]);
     }
   
+    for (let i = 0; i < frames.length; i++) {
+      const currentFrame = frames[i]
+      const scoreForFrame = currentFrame[0] + currentFrame[1]
+  
+      total += scoreForFrame
+    }
+    
     return total;
+  
+    //   return rolls.reduce((score, roll) => score + roll, 0);
   };
   
--- exercises/javascript/test/bowling/bowling.test.js
+++ exercises/javascript/test/bowling/bowling.test.js
@@ -10,4 +10,10 @@ describe("Bowling tests", () => {
     const rolls = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     expect(bowl(rolls)).toBe(20);
   });
+
+  it.skip("should calculate score for a regular game with a single spare", () => {
+    const rolls = [5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(bowl(rolls)).toBe(12);
+  });
+
 });
