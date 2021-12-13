6f9932ce9cab80cbbdc23009f02f2a972795a754
Kallum Jones <kallum.jones@autotrader.co.uk>
2021-12-13 13:16:52 +0000

    Add second test for TDD

--- exercises/javascript/main/bowling/bowling.js
+++ exercises/javascript/main/bowling/bowling.js
@@ -1,3 +1,10 @@
 export const bowl = (rolls) => {
-    return 0;
-}
+    let total = 0;
+    
+    for (let i = 0; i < rolls.length; i++) {
+      total += rolls[i];
+    }
+  
+    return total;
+  };
+  
--- exercises/javascript/test/bowling/bowling.test.js
+++ exercises/javascript/test/bowling/bowling.test.js
@@ -1,7 +1,13 @@
-import { bowl } from '../../main/bowling/bowling'
-describe('Bowling tests', () => {
-  test('Should calculate score of basic bowling game', () => {
+import { bowl } from "../../main/bowling/bowling";
+
+describe("Bowling tests", () => {
+  it("should calculate score of zero when zero pins are hit", () => {
     const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     expect(bowl(rolls)).toBe(0);
+  });
+
+  it("should calculate score for a regular game of bowling (no strikes or spares)", () => {
+    const rolls = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    expect(bowl(rolls)).toBe(20);
   });
 });
