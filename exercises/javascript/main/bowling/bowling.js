56b08a3f45900a62bb532f8acb0876fba9810cc9
Kallum Jones <kallum.jones@autotrader.co.uk>
2021-12-13 16:02:05 +0000

    Add strike test and functionality

--- exercises/javascript/main/bowling/bowling.js
+++ exercises/javascript/main/bowling/bowling.js
@@ -5,22 +5,46 @@ export const bowl = (rolls) => {
     for (let i = 0; i < rolls.length; i += 2) {
       // total += rolls[i];
   
-      frames.push([rolls[i], rolls[i + 1]]);
+      if (rolls[i] === 10) {
+        frames.push([rolls[i]]);
+        i--;
+      } else {
+        frames.push([rolls[i], rolls[i + 1]]);
+        // i++
+      }
     }
   
     for (let i = 0; i < frames.length; i++) {
       const currentFrame = frames[i];
-      let scoreForFrame = currentFrame[0] + currentFrame[1];
+      console.log(currentFrame);
   
-      if (scoreForFrame === 10) {
+      let scoreForFrame;
+  
+      const isStrike = currentFrame.length === 1;
+      // const scoreFromCurrentFrame = isStrike ? currentFrame[0] : currentFrame[0] + currentFrame[1]
+      // const scoreFromNextFrame = isStrike ? nextFrame[0] + nextFrame[1] : nextFrame[0]
+  
+      if (isStrike) {
+        scoreForFrame = currentFrame[0];
+  
         const nextFrame = frames[i + 1];
-        scoreForFrame += nextFrame[0];
+        scoreForFrame += nextFrame[0] + nextFrame[1];
+  
+        console.log(scoreForFrame);
+      } else {
+        scoreForFrame = currentFrame[0] + currentFrame[1];
+        const isSpare = scoreForFrame === 10;
+        const nextFrame = frames[i + 1];
+  
+        if (isSpare) {
+          scoreForFrame += nextFrame[0];
+        }
       }
+  
+      console.log(frames);
   
       total += scoreForFrame;
     }
   
     return total;
-  
-    //   return rolls.reduce((score, roll) => score + roll, 0);
-  };
+}  
--- exercises/javascript/test/bowling/bowling.test.js
+++ exercises/javascript/test/bowling/bowling.test.js
@@ -3,24 +3,33 @@ import { bowl, Game } from "../../main/bowling/bowling";
 describe("Bowling tests", () => {
   it("should calculate score of zero when zero pins are hit", () => {
     const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    expect(bowl(rolls)).toBe(0);
+    const results = bowl(rolls);
+    expect(results).toBe(0);
   });
 
   it("should calculate score for a regular game of bowling (no strikes or spares)", () => {
-    const rolls = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-    expect(bowl(rolls)).toBe(20);
+    const rollsOne = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const resultsOne = bowl(rollsOne);
+    expect(resultsOne).toBe(20);
 
-    const rolls2 = [0, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    expect(bowl(rolls2)).toBe(11);
+    const rollsTwo = [0, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const resultsTwo = bowl(rollsTwo);
+    expect(resultsTwo).toBe(11);
   });
 
-  it("should calculate score for a regular game with a single spare", () => {
+  it("should calculate score for a regular game containing a spare", () => {
     const rolls = [5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    expect(bowl(rolls)).toBe(12);
+    const result = bowl(rolls);
+    expect(result).toBe(12);
+
+    const rolls2 = [5, 5, 1, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const resultsTwo = bowl(rolls2);
+    expect(resultsTwo).toBe(23);
   });
 
-  // it("should calculate score for a regular game of bowling (no strikes or spares)", () => {
-  //   const rolls = [[1, 1], [ 1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]];;
-  //   expect(bowl(rolls)).toBe(20);
-  // });
+  it('Should calculate score for a single strike', () => {
+    const rolls = [10, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const results = bowl(rolls);
+    expect(results).toBe(14);
+  })
 });
