8049d43c0c60f7c0890bd965ce88ce64ad5fd599
Kallum Jones <kallum.jones@autotrader.co.uk>
2021-12-13 11:49:03 +0000

    Add first test pass

---
+++ exercises/javascript/main/bowling/bowling.js
@@ -0,0 +1,3 @@
+export const bowl = (rolls) => {
+    return 0;
+}
--- exercises/javascript/test/bowling/bowling.test.js
+++ exercises/javascript/test/bowling/bowling.test.js
@@ -1,5 +1,7 @@
+import { bowl } from '../../main/bowling/bowling'
 describe('Bowling tests', () => {
-  test('Rename me', () => {
-
+  test('Should calculate score of basic bowling game', () => {
+    const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(bowl(rolls)).toBe(0);
   });
 });
